f811a67418f79b29016a3cda617df74cd3c52195
CricZone Team <nurhossain.bd.nh1@gmail.com>
2026-07-05 21:45:14 +0600

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -1,1 +1,33 @@
+import json
+import subprocess
 
+with open("channels.json", "r", encoding="utf-8") as f:
+    channels = json.load(f)
+
+results = []
+
+for ch in channels:
+    try:
+        cmd = [
+            "yt-dlp",
+            "-g",
+            ch["url"]
+        ]
+
+        stream = subprocess.check_output(
+            cmd,
+            text=True
+        ).strip()
+
+        results.append(
+            f'#EXTINF:-1,{ch["name"]}\n{stream}'
+        )
+
+        print(f'OK : {ch["name"]}')
+
+    except Exception as e:
+        print(e)
+
+with open("output.m3u", "w", encoding="utf-8") as f:
+    f.write("#EXTM3U\n")
+    f.write("\n".join(results))
